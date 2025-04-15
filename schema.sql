fdd82f613ed631507f5a5a2ca76c642aaf943cbc
Roshan Sharma <coderoshan999@gmail.com>
2025-04-15 18:06:46 +0530

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 -- "C:/Program Files/MySQL/MySQL Server 8.0/bin/mysql.exe" -u root -p
--- or just use mysql -u root -p, after setting the bin path of MySQL to environment variables
 -- "system cls" in sql to clear command line log 
--- to bypass on powershell for using "nodemon" command: Set-ExecutionPolicy Bypass -Scope Process
+-- to bypass on powershell: Set-ExecutionPolicy Bypass -Scope Process
 
 USE delta_app;
 CREATE TABLE user(
